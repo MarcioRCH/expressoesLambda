4eb58426c113e531c8756f992acfda3316d1addf
MarcioRCH <marciorchsilva@gmail.com>
2022-09-15 16:22:31 -0300

    First commit

---
+++ src2/entities/Employee.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Employee {
+
+	private String name;
+	private String email;
+	private Double salary;
+	
+	public Employee() {
+		
+	}
+	public Employee(String name, String email, Double salary) {
+		this.email = email;
+		this.name = name;
+		this.salary = salary;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+}
