d6a4ee716a02d7402938f223d3eae58ba0cde3fe
MarcioRCH <marciorchsilva@gmail.com>
2022-09-15 18:16:16 -0300

    SalesAnalysis update

--- src3/application/SalesAnalysis.java
+++ src3/application/SalesAnalysis.java
@@ -1,10 +1,40 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Sale;
+
 public class SalesAnalysis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			List<Sale> list = new ArrayList<>();
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				list.add(new Sale(Integer.parseInt(fields[0]), Integer.parseInt(fields[1]), fields[2], Integer.parseInt(fields[3]), Double.parseDouble(fields[4])));
+				line = br.readLine();
+			}
+			
+		}catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		
+		sc.close();
 	}
 
 }
